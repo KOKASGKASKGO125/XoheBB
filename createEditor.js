92249feece2215a4688c9a31b3b4a215b6add2f3
Egor Udovin <57674283+xoheveras@users.noreply.github.com>
2021-12-17 06:34:05 +0300

    Add files via upload

--- createEditor.js
+++ createEditor.js
@@ -1,13 +1,14 @@
 
 function createEditor()
 {
-	document.getElementById("XoheBB").innerHTML = "<div id='XoheBB-Tools'>Tools<div><textarea id='XoheBB-Editor'></textarea>"
+	document.getElementById("XoheBB").innerHTML = "<div id='XoheBB-Tools'><button>H</button><button><b>B</b>"+
+	"</button><button><i>I</i></button><button><s>S</s></button><div><textarea id='XoheBB-Editor'></textarea>";
 }
 
 function setSize(width, height)
 {
 	document.getElementById("XoheBB").style.width = width+4+"px";
-	document.getElementById("XoheBB").style.height = height+25+"px";
+	document.getElementById("XoheBB").style.height = height+33+"px";
 	document.getElementById("XoheBB-Editor").style.width = width+"px";
 	document.getElementById("XoheBB-Editor").style.height = height+"px";
 	document.getElementById("XoheBB-Tools").style.width = width+4+"px";
